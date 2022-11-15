c40329283c228060429ecb17dd7cd560d6dff075
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2022-11-15 13:11:53 +0100

    Update index.js

--- HttpExample2/index.js
+++ HttpExample2/index.js
@@ -1,12 +1,12 @@
-module.exports = async function (context, req) {
+module.exports = async function (context, teq) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
+    const name = (teq.query.name || (teq.body && teq.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
+    context.tes = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
